9c39330fdf982f76d4f31f0a2b180c4ff519932f
sbgood <sam@sbgood.ca>
2021-04-05 14:26:18 -0400

    add calculate button

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,42 @@
 import { Plugin } from "obsidian";
 import { createArgumentObject } from "./utils";
-import { remove, replace, template, link, command } from "./buttonTypes";
+import {
+  calculate,
+  remove,
+  replace,
+  template,
+  link,
+  command
+} from "./buttonTypes";
 
-//extend the obsidian module with some additional typings
+// extend the obsidian module with some additional typings
 
 export default class ButtonsPlugin extends Plugin {
   async onload(): Promise<void> {
     this.registerMarkdownCodeBlockProcessor("button", async (source, el) => {
       // create an object out of the arguments
       const args = createArgumentObject(source);
-      //handle button clicks
+      // handle button clicks
       const clickHandler = async () => {
-        //handle command buttons
+        // handle command buttons
         if (args.replace) {
           replace(this.app, args);
         }
         if (args.type === "command") {
           command(this.app, args);
         }
-        //handle link buttons
+        // handle link buttons
         if (args.type === "link") {
           link(args);
         }
-        //handle template buttons
+        // handle template buttons
         if (args.type.includes("template")) {
           template(this.app, args);
         }
         if (args.type === "calculate") {
-          const calculationArray = args.action.split(" ");
-          const output = calculationArray.map(async (value) => {
-            if (value.startsWith("l")) {
-              const activeView = this.app.workspace.getActiveViewOfType(
-                MarkdownView
-              );
-              if (activeView) {
-                const file = activeView.file;
-                const originalContent = await this.app.vault.read(file);
-                const arr = originalContent.split("\n");
-                console.log(arr);
-                const lineNumber = parseInt(value.substring(1)) - 1;
-                console.log(lineNumber, arr[lineNumber]);
-                return arr[lineNumber];
-              }
-            }
-            return value;
-          });
-          const resolved = await Promise.all(output);
-          console.log(resolved);
-          const fun = Function(`return ${resolved.join("")}`)();
-          console.log(fun);
-          appendContent(this.app, `Result: ${fun}`, args.name);
+          calculate(this.app, args);
         }
-        //handle removing the button
+        // handle removing the button
         if (args.remove) {
           remove(this.app, args);
         }
