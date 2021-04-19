ff89a68fab56e4a2e044c63424d1dec7fa3f1998
Sam <sam@shbgm.ca>
2021-04-18 21:15:36 -0400

    using redux

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Plugin } from "obsidian";
-import { createArgumentObject, insertButton } from "./utils";
+import { Plugin, TFile, App, MarkdownView } from "obsidian";
+import { insertButton, createArgumentObject } from "./utils";
 import {
   calculate,
   remove,
@@ -8,13 +8,22 @@ import {
   link,
   command,
 } from "./buttonTypes";
-import { createButtonStore, getButtonFromStore } from "./buttonStore";
+import {
+  getButtonFromStore,
+  updateButtonArgs,
+  cycleEvent,
+} from "./buttonStore";
+import { addButtonToStore, addButtonId } from "./parser";
 
 // extend the obsidian module with some additional typings
 
 export default class ButtonsPlugin extends Plugin {
   async onload(): Promise<void> {
-    createButtonStore(this.app);
+    addIdtoButtons(this.app);
+    addAllButtonsToStore(this.app);
+    document.addEventListener("keydown", () => {
+      cycleEvent();
+    });
 
     this.addCommand({
       id: "insert-button-template",
@@ -23,50 +32,87 @@ export default class ButtonsPlugin extends Plugin {
     });
 
     this.registerMarkdownCodeBlockProcessor("button", async (source, el) => {
-      // create an object out of the arguments
-      if (source.includes(" ")) {
-        const sourceArgs = createArgumentObject(source);
-        const buttonObject = await getButtonFromStore(this.app, sourceArgs);
-        const args = buttonObject ? buttonObject.args : sourceArgs;
-        // handle button clicks
-        const clickHandler = async () => {
-          // handle command buttons
-          if (args.replace) {
-            replace(this.app, args);
-          }
-          if (args.type) {
-            if (args.type === "command") {
-              command(this.app, args);
-            }
-            // handle link buttons
-            if (args.type === "link") {
-              link(args);
-            }
-            // handle template buttons
-            if (args.type.includes("template")) {
-              template(this.app, args);
-            }
-            if (args.type === "calculate") {
-              calculate(this.app, args);
-            }
-          }
-          // handle removing the button
-          if (args.remove) {
-            remove(this.app, args);
-          }
-        };
-        //create the button element
-        const button = el.createEl("button", {
-          text: args.name,
-          cls: args.class
-            ? `${args.class} ${args.color}`
-            : `button-default ${args.color ? args.color : ""}`,
-        });
-        args.id ? button.setAttribute("id", args.id) : "";
-        button.on("click", "button", () => {
-          clickHandler();
-        });
+      const createdArgs = createArgumentObject(source);
+      const buttonObj = getButtonFromStore(createdArgs.id);
+      if (!buttonObj) {
+        addIdtoButtons(this.app);
+        cycleEvent();
+        addAllButtonsToStore(this.app);
+        cycleEvent();
       }
+      if (
+        buttonObj &&
+        JSON.stringify(buttonObj.args) !== JSON.stringify(createdArgs)
+      ) {
+        updateButtonArgs(createdArgs, buttonObj.id);
+      }
+      console.log(buttonObj);
+      const args = buttonObj ? buttonObj.args : createdArgs;
+      // create an object out of the arguments
+      // handle button clicks
+      const clickHandler = async () => {
+        // handle command buttons
+        if (args.replace) {
+          replace(this.app, args);
+        }
+        if (args.type) {
+          if (args.type === "command") {
+            command(this.app, args);
+          }
+          // handle link buttons
+          if (args.type === "link") {
+            link(args);
+          }
+          // handle template buttons
+          if (args.type.includes("template")) {
+            template(this.app, args);
+          }
+          if (args.type === "calculate") {
+            calculate(this.app, args);
+          }
+        }
+        // handle removing the button
+        if (args.remove) {
+          remove(this.app, args);
+        }
+      };
+      //create the button element
+      const button = el.createEl("button", {
+        text: args.name,
+        cls: args.class
+          ? `${args.class} ${args.color}`
+          : `button-default ${args.color ? args.color : ""}`,
+      });
+      args.id ? button.setAttribute("id", args.id) : "";
+      button.on("click", "button", () => {
+        clickHandler();
+      });
     });
   }
 }
+
+const addAllButtonsToStore = (app: App) => {
+  const files = app.vault.getMarkdownFiles();
+  files.map(async (file: TFile) => {
+    const text = await app.vault.read(file);
+    addButtonToStore(text, file.path);
+  });
+};
+
+const addIdtoButtons = (app: App) => {
+  const files = app.vault.getMarkdownFiles();
+  files.map(async (file: TFile) => {
+    const text = await app.vault.read(file);
+    const newText = addButtonId(text);
+    await app.vault.modify(file, newText);
+    cycleEvent();
+  });
+};
+
+const getFile = async (app: App): Promise<{ note: string; path: string }> => {
+  const activeView = app.workspace.getActiveViewOfType(MarkdownView);
+  return {
+    note: await app.vault.read(activeView.file),
+    path: activeView.file.path,
+  };
+};
--- src/parser.ts
+++ src/parser.ts
@@ -3,23 +3,16 @@ import parse from "remark-parse";
 import stringify from "remark-stringify";
 import gfm from "remark-gfm";
 import visit from "unist-util-visit";
+import { map } from "unist-util-map";
 import { nanoid } from "nanoid";
 import { createArgumentObject } from "./utils";
-import { Node } from "unist";
 import { Button, Args, ButtonNode } from "./types";
+import { store } from "./buttonStore";
 
 const parser = unified().use(parse).use(gfm);
+const markdowner = unified().use(stringify).use(gfm);
 
-export const returnMD = (tree: Node): string => {
-  return unified().use(stringify).use(gfm).stringify(tree);
-};
-
-export const parseNote = (note: string): Node => {
-  console.log("parsing note: ", note);
-  return parser.parse(note);
-};
-
-export const parseButtons = (note: string, path: string): Button[] => {
+export const addButtonToStore = (note: string, path: string): Button[] => {
   const tree = parser.parse(note);
   const buttons: Button[] = [];
   visit(tree, "code", (node: ButtonNode) => {
@@ -27,27 +20,32 @@ export const parseButtons = (note: string, path: string): Button[] => {
       const value: string = node.value;
       const args: Args = createArgumentObject(value);
       const id = args.id ? args.id : nanoid(6);
-      buttons.push({
-        start: node.position.start.line,
-        end: node.position.end.line,
-        args,
-        path: path,
-        id,
+      store.dispatch({
+        type: "ADD_BUTTON",
+        payload: {
+          start: node.position.start.line,
+          end: node.position.end.line,
+          args,
+          path: path,
+          id,
+        },
       });
     }
   });
   return buttons;
 };
 
-export const removeButton = (tree: Node, id: string): Node => {
-  visit(tree, "code", function (node: ButtonNode, index, parent) {
-    if (node.lang === "button") {
-      if (node.value.includes(id)) {
-        parent.children.splice(index, 1);
-      }
+export const addButtonId = (note: string): string => {
+  const tree = parser.parse(note);
+  const newTree = map(tree, (node: ButtonNode) => {
+    if (node.type == "code" && node.lang === "button") {
+      const value = node.value;
+      const args: Args = createArgumentObject(value);
+      const id = args.id ? args.id : nanoid(6);
+      const newValue = args.id ? value : value.concat("\n", `id ${id}`);
+      return Object.assign({}, node, { value: newValue });
     }
-    // Do not traverse `node`, continue at the node *now* at `index`.
-    return [visit.SKIP, index];
+    return node;
   });
-  return tree;
+  return markdowner.stringify(newTree);
 };
