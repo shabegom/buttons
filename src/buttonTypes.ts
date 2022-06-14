2f4f898783f1063d027759d433039ca0347a4fa2
Alex Peters <alex@peters.net>
2022-06-14 17:20:09 +1000

    remove unneeded logic
    - Use of `await` removes need for `setTimeout`.
    - `cachedData` only ever got `undefined` entries.

--- src/buttonTypes.ts
+++ src/buttonTypes.ts
@@ -263,50 +263,7 @@ export const templater = async (
     const content = await app.vault.cachedRead(file);
     await runTemplater(app);
     const { args } = await getNewArgs(app, position);
-    const cachedData: string[] = [];
-    const cacheChange = app.vault.on("modify", (file) => {
-      cachedData.push(file.unsafeCachedData);
-    });
-    setTimeout(async () => {
-      const button = content
-        .split("\n")
-        .splice(position.lineStart, position.lineEnd - position.lineStart + 2)
-        .join("\n");
-      let finalContent;
-      if (cachedData[0]) {
-        const cachedContent = cachedData[cachedData.length - 1].split("\n");
-        let addOne = false;
-        if (args.type.includes("prepend")) {
-          addOne = true;
-        } else if (args.type.includes("line")) {
-          const lineNumber = args.type.match(/(\d+)/g);
-          if (lineNumber[0]) {
-            const line = parseInt(lineNumber[0]) - 1;
-            if (line < position.lineStart && !args.replace) {
-              addOne = true;
-            }
-          }
-        }
-        if (addOne) {
-          cachedContent.splice(
-            position.lineStart + 1,
-            position.lineEnd - position.lineStart + 2,
-            button
-          );
-        }  else {
-          cachedContent.splice(
-            position.lineStart,
-            position.lineEnd - position.lineStart + 2,
-            button
-          );
-        }
-        finalContent = cachedContent.join("\n");
-      } else {
-        finalContent = content;
-      }
-      await app.vault.modify(file, finalContent);
-      app.metadataCache.offref(cacheChange);
-    }, 200);
+    await app.vault.modify(file, content);
     return args;
   }
 };
