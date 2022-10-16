61a7f26b54f4fa48fa35e3b6cb94928397517e8d
sbgood <sam@sbgood.ca>
2022-10-16 14:13:58 -0400

    make the heart turn red

--- docs/_includes/assets/js/inline.js
+++ docs/_includes/assets/js/inline.js
@@ -1,5 +1,5 @@
 if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
+  window.netlifyIdentity.on("init", (user) => {
     if (!user) {
       window.netlifyIdentity.on("login", () => {
         document.location.href = "/admin/";
@@ -8,42 +8,66 @@ if (window.netlifyIdentity) {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-
+document.addEventListener("DOMContentLoaded", () => {
   const el = document.getElementById("main");
-  el.addEventListener("click", closeNavigation, false)
-
+  el.addEventListener("click", closeNavigation, false);
 });
 
+function heartClick() {
+  const color = document.getElementById("heart").style.color;
+  if (color !== "red") {
+    document.getElementById("heart").style.color = "red";
+  } else {
+    document.getElementById("heart").style.color = "rgb(139, 93, 246)";
+  }
+}
+
 function logout() {
-  localStorage.removeItem("passphrase")
+  localStorage.removeItem("passphrase");
   window.location.href = "/";
 }
 
 function showNavigation() {
   const navigation = document.getElementById("navigation");
-  navigation.classList.remove("hidden", "sticky","pt-32");
-  navigation.classList.add("absolute","right-0", "top-0", "-mt-0", "z-50", "pt-0", "bg-white","border-l", "border-gray-200");
+  navigation.classList.remove("hidden", "sticky", "pt-32");
+  navigation.classList.add(
+    "absolute",
+    "right-0",
+    "top-0",
+    "-mt-0",
+    "z-50",
+    "pt-0",
+    "bg-white",
+    "border-l",
+    "border-gray-200"
+  );
 }
 
 function closeNavigation() {
   const navigation = document.getElementById("navigation");
   navigation.classList.add("hidden");
-  navigation.classList.remove("absolute","right-0","z-50", "bg-gray-100", "border-r", "border-gray-800" );
+  navigation.classList.remove(
+    "absolute",
+    "right-0",
+    "z-50",
+    "bg-gray-100",
+    "border-r",
+    "border-gray-800"
+  );
 }
 
 function toggleLayout(state) {
-  if (localStorage.getItem('layout') === "horizontal") {
-    localStorage.setItem('layout', 'vertical')
-  } else if (localStorage.getItem('layout') === "vertical") {
-    localStorage.setItem('layout', "horizontal")
-  } else if (!localStorage.getItem('layout')) {
+  if (localStorage.getItem("layout") === "horizontal") {
+    localStorage.setItem("layout", "vertical");
+  } else if (localStorage.getItem("layout") === "vertical") {
+    localStorage.setItem("layout", "horizontal");
+  } else if (!localStorage.getItem("layout")) {
     if (state === "horizontal") {
-      localStorage.setItem('layout', 'vertical')
+      localStorage.setItem("layout", "vertical");
     } else {
-      localStorage.setItem('layout', 'horizontal')
+      localStorage.setItem("layout", "horizontal");
     }
   }
 
-  console.log(localStorage.getItem('layout'))
+  console.log(localStorage.getItem("layout"));
 }
